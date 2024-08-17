75c01e3273dc3a72a7515731de124e9a69b1d0be
htjworld <itjhan01@gmail.com>
2024-08-17 16:06:28 +0900

    일반회원가입 레이아웃 구현

--- app/src/main/java/com/example/yesable/SignSelectActivity.kt
+++ app/src/main/java/com/example/yesable/SignSelectActivity.kt
@@ -24,9 +24,10 @@ class SignSelectActivity : AppCompatActivity() {
         // 일반회원가입 화면 전환
         val g_sign = findViewById<Button>(R.id.g_sign_button)
         g_sign.setOnClickListener {
-            val intent = Intent(this, StartActivity::class.java)
+            val intent = Intent(this, IndividualMemberRegistrationActivity::class.java)
             startActivity(intent)
         }
+
 
         // 기업회원가입 화면 전환
         val c_sign = findViewById<Button>(R.id.c_sign_button)
